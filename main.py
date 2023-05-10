540c87b8e0501c821a5d1242c5c796188e62ed10
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-05-10 17:12:25 +0200

    fixed capturing

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ InitPieces(screen, p)
 
 running = True
 drag = 0
+white_turn = True
 while running:
     mouse = pygame.Vector2(pygame.mouse.get_pos())
     event = pygame.event.wait()
@@ -20,27 +21,27 @@ while running:
     if event.type == pygame.MOUSEBUTTONDOWN:
         if drag == 0:
             pos = pieces.get_pos_from_coord(mouse)
-            for i in range(32):
+            for i in range(16, 32) if white_turn else range(16):
                 if p[i].pos == pos:
                     drag = i
                     break
     if event.type == pygame.MOUSEBUTTONUP:
         if drag > 0:
-            for i in range(32):
+            for i in range(16, 32) if not white_turn else range(16):
                 if drag != i:
-                    if p[i].pos == p[drag].pos:
+                    if p[i].pos == pieces.get_pos_from_coord(mouse):
                         print("captured", p[i].type, p[i].pos)
                         p[i].captured = True
                         break
             p[drag].pos = pieces.get_pos_from_coord(mouse)
             p[drag].coord = pieces.get_coord_from_pos(p[drag].pos)
             drag = 0
+            white_turn = not white_turn
     if drag > 0:
         p[drag].coord = mouse - (50, 50)
 
     screen.fill((0, 0, 0))
     Board(screen)
     for i in range(32):
-        if not p[i].captured:
-            p[i].draw()
+        p[i].draw()
     pygame.display.update()

80281307158cb210fe734ad7f8c92f7f84a7b90e
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-05-11 20:42:53 +0200

    fixed chesboard numberings

--- board.py
+++ board.py
@@ -10,10 +10,9 @@ def Board(screen):
     for i in range(length):
         for j in range(length):
             if cnt % 2 == 0:
-                pygame.draw.rect(screen, (154, 87, 26), [size * j + BOARD_POS[0], size * i + BOARD_POS[1], size, size])
+                pygame.draw.rect(screen, (245, 220, 190), [size * j + BOARD_POS[0], size * i + BOARD_POS[1], size, size])
             else:
-                pygame.draw.rect(screen, (245, 220, 190),
-                                 [size * j + BOARD_POS[0], size * i + BOARD_POS[1], size, size])
+                pygame.draw.rect(screen, (154, 87, 26), [size * j + BOARD_POS[0], size * i + BOARD_POS[1], size, size])
             cnt += 1
         cnt -= 1
     pygame.draw.rect(screen, (120, 0, 0), [25, 25, length * size + BOARD_POS[0], length * size + BOARD_POS[0]], 20)
@@ -24,7 +23,7 @@ def InitPieces(screen, p):
             col = 'b'
         else:
             col = 'w'
-        piece = "king"
+        piece = "queen"
         if (8 <= i < 16 or 24 <= i < 32):
             piece = "pawn"
         elif (i == 0 or i == 7 or i == 16 or i == 23):
@@ -34,5 +33,5 @@ def InitPieces(screen, p):
         elif (i == 2 or i == 5 or i == 18 or i == 21):
             piece = "bishop"
         elif (i == 4 or i == 19):
-            piece = "queen"
+            piece = "king"
         p.append(pieces.Piece(screen, f"{col}_{piece}", i + 1 if 0 <= i < 16 else 80 - i))

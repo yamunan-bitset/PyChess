330532e56a17bc8ee745665c2b14de593d73ac80
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-05-20 12:42:43 +0200

    added back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import pieces
 from board import *
+from copy import deepcopy
 
 pygame.init()
 (w, h) = (1000, 900)
@@ -13,9 +14,10 @@ analysis = legal.Legal(board, turn)
 
 running = True
 drag = (-2, -2)
-select_pos1 = (-1, -1)
-select_pos2 = (-1, -1)
+select_pos1 = []
+select_pos2 = []
 found = False
+board_buffer = 0
 while running:
     mouse = pygame.Vector2(pygame.mouse.get_pos())
     event = pygame.event.wait()
@@ -30,19 +32,19 @@ while running:
                         if analysis.board[i][j].type[:1] == ('w' if analysis.white_turn else 'b'):
                             if analysis.board[i][j].pos == pos:
                                 drag = (i, j)
-                                select_pos1 = pos
+                                select_pos1.append(pos)
                                 break
             print("Current pos:", select_pos1)
-            print(analysis.legal(select_pos1))
+            print(analysis.legal(select_pos1[-1]))
 
     if event.type == pygame.MOUSEBUTTONUP:
         if drag[0] >= 0:
-            if analysis.legal(select_pos1) != []:
-                for pos in analysis.legal(select_pos1)[0]:
+            if analysis.legal(select_pos1[-1]) != []:
+                for pos in analysis.legal(select_pos1[-1])[0]:
                     if pos == get_pos_from_coord(mouse):
-                        if analysis.legal(select_pos1)[1] != None:
-                            key = analysis.legal(select_pos1)[1].split()
-                            if pos == analysis.legal(select_pos1)[0][int(key[1])]:
+                        if analysis.legal(select_pos1[-1])[1] != None:
+                            key = analysis.legal(select_pos1[-1])[1].split()
+                            if pos == analysis.legal(select_pos1[-1])[0][int(key[1])]:
                                 if key[0] == "entepassante":
                                     print("entepassante")
                                     analysis.board[int(key[2])][int(key[3])].type = ""
@@ -58,28 +60,43 @@ while running:
                                             analysis.board[3][7 if analysis.white_turn else 0].type = col + "rook"
                                             analysis.board[0][7 if analysis.white_turn else 0].type = ""
                         analysis.history.append(pos)
-                        p = analysis.board[select_pos1[0]][select_pos1[1]]
-                        analysis.board[select_pos1[0]][select_pos1[1]] = pieces.Piece(screen, "", select_pos1)
-                        select_pos2 = get_pos_from_coord(mouse)
-                        p.pos = select_pos2
-                        p.coord = get_coord_from_pos(select_pos2)
-                        analysis.board[select_pos2[0]][select_pos2[1]] = p
+                        p = analysis.board[select_pos1[-1][0]][select_pos1[-1][1]]
+                        analysis.board[select_pos1[-1][0]][select_pos1[-1][1]] = pieces.Piece(screen, "", select_pos1[-1])
+                        select_pos2.append(get_pos_from_coord(mouse))
+                        p.pos = select_pos2[-1]
+                        p.coord = get_coord_from_pos(select_pos2[-1])
+                        analysis.board[select_pos2[-1][0]][select_pos2[-1][1]] = p
                         drag = (-2, -2)
                         analysis.white_turn = not analysis.white_turn
-                        print(select_pos2)
+                        print(select_pos2[-1])
                         found = True
                         break
-                analysis.board[select_pos1[0]][select_pos1[1]].pos = select_pos1
-                analysis.board[select_pos1[0]][select_pos1[1]].coord = get_coord_from_pos(select_pos1)
+                analysis.board[select_pos1[-1][0]][select_pos1[-1][1]].pos = select_pos1[-1]
+                analysis.board[select_pos1[-1][0]][select_pos1[-1][1]].coord = get_coord_from_pos(select_pos1[-1])
                 drag = (-2, -2)
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            try:
+                p = analysis.board[select_pos2[-1][0]][select_pos2[-1][1]].type
+                analysis.board[select_pos2[-1][0]][select_pos2[-1][1]].type = analysis.board[select_pos1[-1][0]][select_pos1[-1][1]].type
+                analysis.board[select_pos1[-1][0]][select_pos1[-1][1]].type = p
+                analysis.white_turn = not analysis.white_turn
+                print(select_pos1)
+                print(select_pos2)
+                select_pos1 = select_pos1[:-1]
+                select_pos2 = select_pos2[:-1]
+                print(select_pos1)
+                print(select_pos2)
+            except IndexError:
+                pass
 
     screen.fill((0, 0, 0))
     Board(screen)
 
-    if select_pos1[0] >= 0:
-        pygame.draw.rect(screen, (220, 190, 0), (get_coord_from_pos(select_pos1)[0], get_coord_from_pos(select_pos1)[1], 100, 100))
-    if select_pos2[0] >= 0:
-        pygame.draw.rect(screen, (220, 190, 0), (get_coord_from_pos(select_pos2)[0], get_coord_from_pos(select_pos2)[1], 100, 100))
+    if select_pos1 != [] and select_pos2 != []:
+        pygame.draw.rect(screen, (220, 190, 0), (get_coord_from_pos(select_pos1[-1])[0], get_coord_from_pos(select_pos1[-1])[1], 100, 100))
+        pygame.draw.rect(screen, (220, 190, 0), (get_coord_from_pos(select_pos2[-1])[0], get_coord_from_pos(select_pos2[-1])[1], 100, 100))
 
     if drag[0] >= 0:
         moves = analysis.legal(drag)
@@ -88,7 +105,7 @@ while running:
             s.set_alpha(128)
             s.fill((255, 0, 0))
             screen.blit(s, get_coord_from_pos(move))
-        board[drag[0]][drag[1]].coord = mouse - BOARD_POS
+        analysis.board[drag[0]][drag[1]].coord = mouse - BOARD_POS
         if get_pos_from_coord(mouse)[0] != -1 and get_pos_from_coord(mouse)[1] != -1:
             pygame.draw.rect(screen, (255, 255, 255, 50), (get_coord_from_pos(get_pos_from_coord(mouse))[0], get_coord_from_pos(get_pos_from_coord(mouse))[1], 100, 100), 5)
 
